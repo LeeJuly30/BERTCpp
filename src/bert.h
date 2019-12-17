2253511752cf9e918f938e0f4016d3c427f4f9cb
leeflow <leejuly30@gmail.com>
2019-12-17 19:02:43 +0800

    all construster explicit

--- src/bert.h
+++ src/bert.h
@@ -9,7 +9,7 @@ namespace lh{
     class Bert{
 
         public:
-            Bert(std::vector<std::string> names, Graph<T> &pb_graph, std::size_t pre_batch_size, std::size_t pre_seq_len, std::size_t embedding_size, std::size_t num_heads, std::size_t head_hidden_size, std::size_t intermediate_ratio, std::size_t num_layers);
+            explicit Bert(std::vector<std::string> names, Graph<T> &pb_graph, std::size_t pre_batch_size, std::size_t pre_seq_len, std::size_t embedding_size, std::size_t num_heads, std::size_t head_hidden_size, std::size_t intermediate_ratio, std::size_t num_layers);
             ~Bert();
             void compute(std::size_t batch_size, std::size_t seq_len, uint64_t* token_input, uint64_t* posit_input, uint64_t* type_input, uint64_t* mask, T* output);
 
--- src/transformer.h
+++ src/transformer.h
@@ -9,7 +9,7 @@ namespace lh
     template<class T>
     class Transformer{
         public:
-            Transformer(std::vector<std::string> names, Graph<T> &pb_graph, std::size_t pre_batch_size, std::size_t pre_seq_len, std::size_t num_heads, std::size_t head_hidden_size, std::size_t intermediate_ratio, std::size_t num_layers);
+            explicit Transformer(std::vector<std::string> names, Graph<T> &pb_graph, std::size_t pre_batch_size, std::size_t pre_seq_len, std::size_t num_heads, std::size_t head_hidden_size, std::size_t intermediate_ratio, std::size_t num_layers);
             ~Transformer();
             void compute(std::size_t batch_size, std::size_t seq_len, T* input, uint64_t* mask, T* output);
         
